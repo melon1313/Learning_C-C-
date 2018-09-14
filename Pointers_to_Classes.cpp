481417d0f99dac24f984766ea9f10aea2befda53
chris <chris831127@yahoo.com.tw>
2018-09-14 15:36:49 +0800

    20180914

--- Pointers_to_Classes.cpp
+++ Pointers_to_Classes.cpp
@@ -1,32 +1,32 @@
 #include<iostream>
 using namespace std;
 
-class CREctangle{
+class Rectangle{
     private:
         int weight;
         int hight;
     
     public:
         void set_value(int, int);
-        int get_area(void){ return (weight * hight);}
+        int get_area(void){return (weight * hight);}
 };
 
-void CREctangle::set_value(int a, int b){
+void Rectangle::set_value(int a, int b){
     weight = a;
     hight = b;
 }
 
 int main(){
-    CREctangle a, *b, *c;
-    CREctangle *d = new CREctangle[2];
-
-    b = new CREctangle;
+    Rectangle a, *b, *c;
+    Rectangle *d = new Rectangle[2];
+    
+    b = new Rectangle;
     c = &a;
 
     a.set_value(3, 4);
-    b -> set_value(5, 6);
-    d -> set_value(7, 8);
-    d[1].set_value(8, 9);
+    b -> set_value(4, 5);
+    d -> set_value(5, 6);
+    d[1].set_value(6, 7);
 
     cout << a.get_area() << endl;
     cout << b -> get_area() << endl;
